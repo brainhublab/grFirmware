0a0ea842461e877c4e202e2bfad5ab607833ab3a
Oleg <o.kamenscic@gmail.com>
2016-07-10 17:16:03 +0300

    remove net useful fles, add functionality to collect data from sensors and sedn them via bluetooth

--- sermo_glove/sermo_glove.ino
+++ sermo_glove/sermo_glove.ino
@@ -1,58 +1,163 @@
+/*include arduino libs*/
 #include <Arduino.h>
 #include <SPI.h>
 #if not defined (_VARIANT_ARDUINO_DUE_X_) && not defined (_VARIANT_ARDUINO_ZERO_)
   #include <SoftwareSerial.h>
 #endif
 
+/*including bluetooth libs and config header*/
 #include "Adafruit_BLE.h"
 #include "Adafruit_BluefruitLE_SPI.h"
 #include "Adafruit_BluefruitLE_UART.h"
 
 #include "BluefruitConfig.h"
-
-#include <L3G.h>
-
+/*including compas acclerometer and gyro */
 #include <Wire.h>
 #include <LSM303.h>
-
-LSM303 compass;
+#include <L3G.h>
+LSM303 compass;//define compas
 L3G gyro;
 
-char report[80];
+//containers for data report
+char report_gyro_and_compass[80];
 
-void setup()
+
+/*=========================================================================
+    MINIMUM_FIRMWARE_VERSION  Minimum firmware version to have some new features
+    MODE_LED_BEHAVIOUR        LED activity, valid options are
+                              "DISABLE" or "MODE" or "BLEUART" or
+                              "HWUART"  or "SPI"  or "MANUAL"
+    -----------------------------------------------------------------------*/
+    #define FACTORYRESET_ENABLE         1
+    #define MINIMUM_FIRMWARE_VERSION    "0.6.6"
+    #define MODE_LED_BEHAVIOUR          "MODE"
+/*=========================================================================*/
+
+// Create the bluefruit object, either software serial...uncomment these lines
+
+SoftwareSerial bluefruitSS = SoftwareSerial(BLUEFRUIT_SWUART_TXD_PIN, BLUEFRUIT_SWUART_RXD_PIN);
+
+Adafruit_BluefruitLE_UART ble(bluefruitSS, BLUEFRUIT_UART_MODE_PIN,
+                      BLUEFRUIT_UART_CTS_PIN, BLUEFRUIT_UART_RTS_PIN);
+
+// A small helper
+void error(const __FlashStringHelper*err) {
+  Serial.println(err);
+  while (1);
+}
+
+/**************************************************************************/
+/*!
+    @brief  Sets up the HW an the BLE module (this function is called
+            automatically on startup)
+*/
+/**************************************************************************/
+void setup(void)
 {
-  Serial.begin(9600);
+  while (!Serial);  // required for Flora & Micro
+  delay(500);
+  Serial.begin(115200);
+
+  //init compas data
   Wire.begin();
   compass.init();
   compass.enableDefault();
+
+  //init gyro data
   if (!gyro.init())
   {
     Serial.println("Failed to autodetect gyro type!");
     while (1);
   }
+
   gyro.enableDefault();
+
+  Serial.println(F("Adafruit Bluefruit Command <-> Data Mode Example"));
+  Serial.println(F("------------------------------------------------"));
+
+  /* Initialise the module */
+  Serial.print(F("Initialising the Bluefruit LE module: "));
+
+  if ( !ble.begin(VERBOSE_MODE) )
+  {
+    error(F("Couldn't find Bluefruit, make sure it's in CoMmanD mode & check wiring?"));
+  }
+  Serial.println( F("OK!") );
+
+  if ( FACTORYRESET_ENABLE )
+  {
+    /* Perform a factory reset to make sure everything is in a known state */
+    Serial.println(F("Performing a factory reset: "));
+    if ( ! ble.factoryReset() ){
+      error(F("Couldn't factory reset"));
+    }
+  }
+
+  /* Disable command echo from Bluefruit */
+  ble.echo(false);
+
+  Serial.println("Requesting Bluefruit info:");
+  /* Print Bluefruit information */
+  ble.info();
+
+  Serial.println(F("Please use Adafruit Bluefruit LE app to connect in UART mode"));
+  Serial.println(F("Then Enter characters to send to Bluefruit"));
+  Serial.println();
+
+  ble.verbose(false);  // debug info is a little annoying after this point!
+
+  /* Wait for connection */
+  while (! ble.isConnected()) {
+      delay(500);
+  }
+
+  Serial.println(F("******************************"));
+
+  // LED Activity command is only supported from 0.6.6
+  if ( ble.isVersionAtLeast(MINIMUM_FIRMWARE_VERSION) )
+  {
+    // Change Mode LED Activity
+    Serial.println(F("Change LED activity to " MODE_LED_BEHAVIOUR));
+    ble.sendCommandCheckOK("AT+HWModeLED=" MODE_LED_BEHAVIOUR);
+  }
+
+  // Set module to DATA mode
+  Serial.println( F("Switching to DATA mode!") );
+  ble.setMode(BLUEFRUIT_MODE_DATA);
+
+  Serial.println(F("******************************"));
 }
 
-void loop()
+/**************************************************************************/
+/*!
+    @brief  Constantly poll for new command or response data
+*/
+/**************************************************************************/
+void loop(void)
 {
+  //read and organize compas data
   compass.read();
-
-  snprintf(report, sizeof(report), "A: %6d %6d %6d    M: %6d %6d %6d",
+  gyro.read();
+  snprintf(report_gyro_and_compass, sizeof(report_gyro_and_compass), "A: %6d %6d %6d    M: %6d %6d %6d  G-XYZ: %6d %6d %6d",
     compass.a.x, compass.a.y, compass.a.z,
-    compass.m.x, compass.m.y, compass.m.z);
-  Serial.println(report);
-
-   gyro.read();
-
-  Serial.print("G ");
-  Serial.print("X: ");
-  Serial.print((int)gyro.g.x);
-  Serial.print(" Y: ");
-  Serial.print((int)gyro.g.y);
-  Serial.print(" Z: ");
-  Serial.println((int)gyro.g.z);
-
-
+    compass.m.x, compass.m.y, compass.m.z,
+    gyro.g.x, gyro.g.y, gyro.g.z);
+    
+  Serial.println(report_gyro_and_compass);
+  ble.print(report_gyro_and_compass);//put data in bluetooth stream
   delay(100);
+
+  // Echo received data
+  while ( ble.available() )
+  {
+    int c = ble.read();
+
+    Serial.print((char)c);
+
+    // Hex output too, helps w/debugging!
+    Serial.print(" [0x");
+    if (c <= 0xF) Serial.print(F("0"));
+    Serial.print(c, HEX);
+    Serial.print("] ");
+  }
 }
