c2e55054a1daf4f7f089b4a26db7fa4829937eec
Oleg <o.kamenscic@gmail.com>
2017-04-02 11:29:55 +0300

    fix output

--- grRfCom/MinIMU9AHRS/Output.ino
+++ grRfCom/MinIMU9AHRS/Output.ino
@@ -1,25 +1,26 @@
 void printdata(uint8_t i)
 {
-      Serial.print("!");
+      //Serial.print("!");
       Serial.print(i);
+     
 
-      Serial.print(",AN:");
+      Serial.print(" ");
       Serial.print(SENSORS[i].AN[0]);
-      Serial.print(",");
+      Serial.print(" ");
       Serial.print(SENSORS[i].AN[1]);
-      Serial.print(",");
+      Serial.print(" ");
       Serial.print(SENSORS[i].AN[2]);
-      Serial.print(",");
+      Serial.print(" ");
       Serial.print(SENSORS[i].AN[3]);
-      Serial.print (",");
+      Serial.print (" ");
       Serial.print(SENSORS[i].AN[4]);
-      Serial.print (",");
+      Serial.print (" ");
       Serial.print(SENSORS[i].AN[5]);
-      Serial.print(",");
+      Serial.print(" ");
       Serial.print(SENSORS[i].magnetom_x);
-      Serial.print (",");
+      Serial.print (" ");
       Serial.print(SENSORS[i].magnetom_y);
-      Serial.print (",");
+      Serial.print (" ");
       Serial.print(SENSORS[i].magnetom_z);
-      Serial.println();
+      Serial.print("\n");
 }
