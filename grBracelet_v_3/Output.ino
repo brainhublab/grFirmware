1ed9bc5b7a8536a5b2047e84312af25a5445a8df
Oleg <o.kamenscic@gmail.com>
2017-09-09 12:20:20 +0300

    add new version and make hc-06 config sketch

---
+++ grBracelet_v_3/Output.ino
@@ -0,0 +1,54 @@
+void printdata(uint8_t i)
+{
+  if(i==5)
+  {
+    Serial.print(i);
+    Serial.print(" ");
+    Serial.print(SENSORS[i].gyro_x);
+    Serial.print(" ");
+    Serial.print(SENSORS[i].gyro_y);
+    Serial.print(" ");
+    Serial.print(SENSORS[i].gyro_z);
+    Serial.print(" ");
+    Serial.print(SENSORS[i].accel_x);
+    Serial.print (" ");
+    Serial.print(SENSORS[i].accel_y);
+    Serial.print (" ");
+    Serial.print(SENSORS[i].accel_z);
+    Serial.print(" ");
+    Serial.print(SENSORS[i].magnetom_x);
+    Serial.print (" ");
+    Serial.print(SENSORS[i].magnetom_y);
+    Serial.print (" ");
+    Serial.print(SENSORS[i].magnetom_z);
+    Serial.print(" ");
+    Serial.print(SENSORS[i].timestamp);
+    //Serial.print('\n');
+    Serial.print('\n');
+  }
+  else
+  {
+    bSerial.print(i);
+    bSerial.print(" ");
+    bSerial.print(SENSORS[i].gyro_z*0); // y
+    bSerial.print(" ");
+    bSerial.print(SENSORS[i].gyro_x*0); // z
+    bSerial.print(" ");
+    bSerial.print(SENSORS[i].gyro_y);
+    bSerial.print(" ");
+    bSerial.print(SENSORS[i].accel_z*0);
+    bSerial.print (" ");
+    bSerial.print(SENSORS[i].accel_x*0);
+    bSerial.print (" ");
+    bSerial.print(SENSORS[i].accel_y);
+    bSerial.print(" ");
+    bSerial.print(SENSORS[i].magnetom_z*0);
+    bSerial.print (" ");
+    bSerial.print(SENSORS[i].magnetom_x*0);
+    bSerial.print (" ");
+    bSerial.print(SENSORS[i].magnetom_y);
+    bSerial.print(" ");
+    bSerial.print(SENSORS[i].timestamp);
+    bSerial.print('\n');
+  }
+}
---
+++ grBracelet_v_3/sensor.h
@@ -0,0 +1,16 @@
+struct sensor {
+  float timestamp;
+  int A_AN[3];
+  int A_AN_OFFSET[3]={0, 0, 0};
+  int G_AN[3];
+  int G_AN_OFFSET[3]={0, 0, 0};
+  int gyro_x;
+  int gyro_y;
+  int gyro_z;
+  int accel_x;
+  int accel_y;
+  int accel_z;
+  int magnetom_x;
+  int magnetom_y;
+  int magnetom_z;
+};
